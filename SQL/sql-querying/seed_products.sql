a1da72fb29fade1326bee1afd9cbe78cfe7ba119
Konnie Wang <wangkonnie@gmail.com>
2022-03-03 05:02:51 -0500

    updating sql notes

--- SQL/sql-querying/seed_products.sql
+++ SQL/sql-querying/seed_products.sql
@@ -5,22 +5,25 @@ CREATE DATABASE products_db;
 \c products_db
 
 CREATE TABLE products (
-  id SERIAL PRIMARY KEY, --SERIAL means it'll auto increment id without me needing to manually input them
+  id SERIAL PRIMARY KEY, --SERIAL means it'll auto increment id without needing to manually input them, PRIMARY KEY is unique ID for each row
   name TEXT NOT NULL,    --NOT NULL means you can't leave them blank
   price FLOAT CHECK (price > 0),
   can_be_returned BOOLEAN NOT NULL
 );
 
---is there a way for the psql terminal to automatically read the data in this file?
---or do I need to manually input it?
--- INSERT INTO products 
---   (name, price, can_be_returned)
--- VALUES 
---   ('cheese', 5.00, 'f'); 
 
 
+
+-- to check if postgresql is working: sudo service postgresql status
+-- to activate it: sudo service postgresql start
 -- To start up terminal type in psql
 -- \l to see what tables there are
+--  To seed your database, run the following command from within the starter code directory.
+-- cd into the main folder directory where the seed files are held but don't activate psql yet, instead type the seed command line
+-- seed a database file: psql < my_database.sql
 -- \c _dbname_ to connect to specific ones
 -- SELECT * FROM _nameoftable_; to get data
 -- \q to exit and return to main terminal page
+-- to read a .sql table file from the terminal make sure you're cd in the right folder then psql and connect to specific databases
+ -- then type \i name_of_file_in the same folder.sql, it will show you all the table info from that file
+-- pg_dump -C -c -O my_database_name > backup.sql to make a verbose file of your terminal table

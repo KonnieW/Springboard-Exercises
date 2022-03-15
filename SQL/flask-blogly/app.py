7bbf7202dea86999dab3910eb6d9486bf847b307
Konnie Wang <wangkonnie@gmail.com>
2022-03-15 00:40:27 -0400

    updating blogly

--- SQL/flask-blogly/app.py
+++ SQL/flask-blogly/app.py
@@ -2,11 +2,18 @@
 
 from flask import Flask
 from models import db, connect_db
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET KEY'] = "SECRETSSSS"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+db = SQLAlchemy()
+db.app = app
+db.init_app(app)
+
 connect_db(app)
 db.create_all()
+
